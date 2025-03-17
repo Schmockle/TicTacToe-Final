0352ea2bd2aef77a14c66b0bd1ac37ca48fd2c0f
Metrizz1 <kevinlunde2004@gmail.com>
2025-03-17 14:21:18 +0100

    change the color on X to blue and O to red

--- src/square.jsx
+++ src/square.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
 export default function Square({ value, onSquareClick }) {
+  // Bestem ekstra klasse basert på verdien (X eller O)
+  const extraClass = value === 'X' ? 'x' : value === 'O' ? 'o' : '';
+
   return (
-    <button className="square" onClick={onSquareClick}>
+    <button className={`square ${extraClass}`} onClick={onSquareClick}>
       {value}
     </button>
   );
